9b80edf3bab8c3f0f5cb187c5356cfe9bbb2660e
ssoares-dev <ssoares@citeve.pt>
2025-06-24 16:22:53 +0100

    Day 1

---
+++ finance-tracker/src/Pages/Dashboard/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Dashboard: React.FC = () => {
+  return (
+    <div>
+      <h1>Dashboard</h1>
+      <p>Welcome to your finance tracker dashboard!</p>
+      {/* Add more components or features here */}
+    </div>
+  );
+}
+export default Dashboard;
---
+++ finance-tracker/src/Pages/List/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const List: React.FC = () => {
+  return (
+    <div>
+      <h1>Transaction List</h1>
+      <p>Here you can view all your transactions.</p>
+      {/* Add more components or features here */}
+    </div>
+  );
+};
+export default List;
